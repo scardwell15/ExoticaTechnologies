e45da8b0b70be356d82d31d81484f1b77d2abbe2
scardwell <scardwell15@gmail.com>
2023-09-23 16:48:49 -0600

    added music by haplogynae. hopefully fixed some refit bugs in the mean time as well. hopefully fixed some issues with detecting all phase ships on phasefield engine

--- src/exoticatechnologies/modifications/conditions/impl/HasPhase.kt
+++ src/exoticatechnologies/modifications/conditions/impl/HasPhase.kt
@@ -11,10 +11,12 @@ import exoticatechnologies.modifications.conditions.OperatorCondition
 class HasPhase : OperatorCondition() {
     override val key = "hasPhase"
     override fun getActual(member: FleetMemberAPI, mods: ShipModifications?, variant: ShipVariantAPI): Any? {
-        return member.hullSpec.hints.contains(ShipHullSpecAPI.ShipTypeHints.PHASE) || variant.hints.contains(ShipHullSpecAPI.ShipTypeHints.PHASE)
+        return member.hullSpec.hints.contains(ShipHullSpecAPI.ShipTypeHints.PHASE)
+                || variant.hints.contains(ShipHullSpecAPI.ShipTypeHints.PHASE)
                 || member.hullSpec.isPhase
                 || (member.variant.hullSpec.shipSystemId != null
-                && Global.getSettings().getShipSystemSpec(member.variant.hullSpec.shipSystemId) != null
-                && Global.getSettings().getShipSystemSpec(member.variant.hullSpec.shipSystemId).isPhaseCloak)
+                && Global.getSettings().getShipSystemSpec(member.variant.hullSpec.shipSystemId)?.isPhaseCloak == true)
+                || (member.variant.hullSpec.shipDefenseId != null
+                && Global.getSettings().getShipSystemSpec(member.variant.hullSpec.shipDefenseId)?.isPhaseCloak == true)
     }
 }
